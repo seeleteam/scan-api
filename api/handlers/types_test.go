6550adaee562117305d9d67d1958b728f6ba0fb9
zheng wei <408212402@qq.com>
2018-09-12 15:41:28 +0800

    types_test

--- api/handlers/types_test.go
+++ api/handlers/types_test.go
@@ -16,13 +16,19 @@ func newTestDBBlock(t *testing.T) *database.DBBlock {
 	return &database.DBBlock{
 		Reward: 99,
 		Height: 133853,
+		Txs:    []database.DBSimpleTxInBlock{{Fee: 99}},
 	}
 }
 func Test_CreateRetSimpleBlockInfo(t *testing.T) {
+	var TxFee int64
 	header := newTestDBBlock(t)
 	got := createRetSimpleBlockInfo(header)
+
+	for i := 0; i < len(header.Txs); i++ {
+		TxFee += header.Txs[i].Fee
+	}
+	assert.Equal(t, got.Fee, TxFee)
 	gots := int64(got.Height)
 	assert.Equal(t, gots, header.Height)
 	assert.Equal(t, got.Reward, header.Reward)
-
 }
