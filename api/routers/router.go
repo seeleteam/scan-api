c08368a52a62586fde705e18877a3fd8e827e04a
408212402@qq.com <408212402@qq.com>
2018-10-12 14:01:44 +0800

    test1012

--- api/routers/router.go
+++ api/routers/router.go
@@ -49,7 +49,7 @@ func (r *Router) Init(e *gin.Engine) {
 	v1.GET("/Homeaccounts", r.AccountHandler.GetHomeAccounts())
 	v1.GET("/txs", r.BlockHandler.GetTxs())
 	v1.GET("/pendingtxs", r.BlockHandler.GetPendingTxs())
-	v1.GET("blockTxsTps", r.BlockHandler.GetBlockTxsTps())
+	v1.GET("/blockTxsTps", r.BlockHandler.GetBlockTxsTps())
 	v1.GET("/tx", r.BlockHandler.GetTxByHash())
 	v1.GET("/blockprotime", r.BlockHandler.GetBlockProtime())
 	//ugly fix this
