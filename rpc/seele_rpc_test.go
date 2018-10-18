16dd9053803e0d639186b81d9010a2b6774323e2
forjoin92 <forjoin92@gmail.com>
2018-10-18 15:53:20 +0800

    improve unit-test
    UT use "github.com/stretchr/testify/assert"

--- rpc/seele_rpc_test.go
+++ rpc/seele_rpc_test.go
@@ -9,6 +9,8 @@ import (
 	"encoding/json"
 	"math/big"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetPeerInfos(t *testing.T) {
@@ -50,39 +52,25 @@ func TestGetPeerInfos(t *testing.T) {
 	peersMp := make([]map[string]interface{}, 0)
 
 	peers := getPeerInfos(peersMp)
-	if len(peers) != 0 {
-		t.Fatal("getPeerInfos get wrong peers number")
-	}
+	assert.Equal(t, len(peers), 0)
 
 	json.Unmarshal([]byte(peersJSON), &peersMp)
 
 	peers = getPeerInfos(peersMp)
-	if len(peers) != len(peersMp) {
-		t.Fatal("parseToPeerInfos failed")
-	}
+	assert.Equal(t, len(peers), len(peersMp))
 	for _, info := range peers {
 		if info.ID == "0x0ea2a45ab5a909c309439b0e004c61b7b2a3e831" {
-			for _, cap := range info.Caps {
-				if cap != "lightSeele/1" && cap != "seele/1" {
-					t.Fatal("getPeerInfos get wrong data")
-				}
-			}
-			if info.ShardNumber != 2 ||
-				info.LocalAddress != "127.0.0.1:55239" ||
-				info.RemoteAddress != "127.0.0.1:8058" {
-				t.Fatal("getPeerInfos get wrong data")
-			}
+			assert.Contains(t, info.Caps, "lightSeele/1")
+			assert.Contains(t, info.Caps, "seele/1")
+			assert.Equal(t, info.ShardNumber, 2)
+			assert.Equal(t, info.LocalAddress, "127.0.0.1:55239")
+			assert.Equal(t, info.RemoteAddress, "127.0.0.1:8058")
 		} else if info.ID == "0x0da2a45ab5a909c309439b0e004c61b7b2a3e831" {
-			for _, cap := range info.Caps {
-				if cap != "lightSeele/1" && cap != "seele/1" {
-					t.Fatal("getPeerInfos get wrong data")
-				}
-			}
-			if info.ShardNumber != 1 ||
-				info.LocalAddress != "127.0.0.1:55239" ||
-				info.RemoteAddress != "127.0.0.1:8058" {
-				t.Fatal("getPeerInfos get wrong data")
-			}
+			assert.Contains(t, info.Caps, "lightSeele/1")
+			assert.Contains(t, info.Caps, "seele/1")
+			assert.Equal(t, info.ShardNumber, 1)
+			assert.Equal(t, info.LocalAddress, "127.0.0.1:55239")
+			assert.Equal(t, info.RemoteAddress, "127.0.0.1:8058")
 		}
 	}
 
@@ -98,12 +86,8 @@ func TestGetBalance(t *testing.T) {
 	json.Unmarshal([]byte(balanceJSON), &balanceMp)
 
 	balance, err := getBalance(balanceMp, "0x4c10f2cd2159bb432094e3be7e17904c2b4aeb21")
-	if err != nil {
-		t.Fatal("parseToBalance failed")
-	}
-	if balance != 261899990000 {
-		t.Fatal("parseToBalance failed")
-	}
+	assert.Equal(t, err, nil)
+	assert.Equal(t, balance, int64(261899990000))
 }
 
 func TestGetReceiptByTxHash(t *testing.T) {
@@ -121,15 +105,14 @@ func TestGetReceiptByTxHash(t *testing.T) {
 	json.Unmarshal([]byte(receiptJSON), &receiptMp)
 
 	receipt := getReceiptByTxHash(receiptMp)
-	if receipt.ContractAddress != "0x" ||
-		receipt.UsedGas.Cmp(big.NewInt(21000)) != 0 ||
-		receipt.Failed != false ||
-		receipt.PostState != "0xdd0b0fc6605bbb2e76b8c22ccd466ea5eaa1a80e4860fbdf971be58ded3d782b" ||
-		receipt.Result != "0x" ||
-		receipt.TotalFee.Cmp(big.NewInt(21000)) != 0 ||
-		receipt.TxHash != "0xbd2ca4f9869c714e589ad6a3b16731c8cb066de40d0e27e220cc1e014577baff" {
-		t.Fatal("parseToReceipt failed")
-	}
+
+	assert.Equal(t, receipt.ContractAddress, "0x")
+	assert.Equal(t, receipt.UsedGas.Cmp(big.NewInt(21000)), 0)
+	assert.Equal(t, receipt.Failed, false)
+	assert.Equal(t, receipt.PostState, "0xdd0b0fc6605bbb2e76b8c22ccd466ea5eaa1a80e4860fbdf971be58ded3d782b")
+	assert.Equal(t, receipt.Result, "0x")
+	assert.Equal(t, receipt.TotalFee.Cmp(big.NewInt(21000)), 0)
+	assert.Equal(t, receipt.TxHash, "0xbd2ca4f9869c714e589ad6a3b16731c8cb066de40d0e27e220cc1e014577baff")
 }
 
 func TestGetPendingTransactions(t *testing.T) {
@@ -163,27 +146,23 @@ func TestGetPendingTransactions(t *testing.T) {
 	txs := getPendingTransactions(pendingTxsMp)
 	for _, tx := range txs {
 		if tx.Hash == "0xc3a8be67dbfe3fc8f9478d91fb49610368515460ad25cba3f566bdf329cdfec6" {
-			if tx.AccountNonce != 3 ||
-				tx.Amount.Cmp(big.NewInt(10000)) != 0 ||
-				tx.From != "0x4c10f2cd2159bb432094e3be7e17904c2b4aeb21" ||
-				tx.To != "0xddada93f414f5063cbd4cb642705b1c848fa3c01" ||
-				tx.GasLimit != 21000 ||
-				tx.GasPrice != 1 ||
-				tx.Payload != "" ||
-				tx.Timestamp != 1 {
-				t.Fatal("getPendingTransactions get wrong data")
-			} else if tx.Hash == "0xd3a8be67dbfe3fc8f9478d91fb49610368515460ad25cba3f566bdf329cdfec6" {
-				if tx.AccountNonce != 4 ||
-					tx.Amount.Cmp(big.NewInt(20000)) != 0 ||
-					tx.From != "0x4c10f2cd2159bb432094e3be7e17904c2b4aeb21" ||
-					tx.To != "0xddada93f414f5063cbd4cb642705b1c848fa3c01" ||
-					tx.GasLimit != 31000 ||
-					tx.GasPrice != 1 ||
-					tx.Payload != "382342" ||
-					tx.Timestamp != 2 {
-					t.Fatal("getPendingTransactions get wrong data")
-				}
-			}
+			assert.Equal(t, tx.AccountNonce, uint64(3))
+			assert.Equal(t, tx.Amount.Cmp(big.NewInt(10000)), 0)
+			assert.Equal(t, tx.From, "0x4c10f2cd2159bb432094e3be7e17904c2b4aeb21")
+			assert.Equal(t, tx.To, "0xddada93f414f5063cbd4cb642705b1c848fa3c01")
+			assert.Equal(t, tx.GasLimit, int64(21000))
+			assert.Equal(t, tx.GasPrice, int64(1))
+			assert.Equal(t, tx.Payload, "")
+			assert.Equal(t, tx.Timestamp, uint64(1))
+		} else if tx.Hash == "0xd3a8be67dbfe3fc8f9478d91fb49610368515460ad25cba3f566bdf329cdfec6" {
+			assert.Equal(t, tx.AccountNonce, uint64(4))
+			assert.Equal(t, tx.Amount.Cmp(big.NewInt(20000)), 0)
+			assert.Equal(t, tx.From, "0x4c10f2cd2159bb432094e3be7e17904c2b4aeb21")
+			assert.Equal(t, tx.To, "0xddada93f414f5063cbd4cb642705b1c848fa3c01")
+			assert.Equal(t, tx.GasLimit, int64(31000))
+			assert.Equal(t, tx.GasPrice, int64(1))
+			assert.Equal(t, tx.Payload, "382342")
+			assert.Equal(t, tx.Timestamp, uint64(2))
 		}
 	}
 }
