12ad0d824773612f3040923d56efafea5684f3e7
lizhengwei <41619423+lizhengwei88@users.noreply.github.com>
2018-11-12 16:57:53 +0800

    Repair account assets accounted for (#91)
    * accounts
    
    * homeaccounts

--- api/handlers/types.go
+++ api/handlers/types.go
@@ -117,8 +117,9 @@ type RetSimpleAccountInfo struct {
 
 //RetSimpleAccountHome
 type RetSimpleAccountHome struct {
-	Address string `json:"address"`
-	Balance int64  `json:"balance"`
+	Address    string  `json:"address"`
+	Balance    int64   `json:"balance"`
+	Percentage float64 `json:"percentage"`
 }
 
 //RetDetailAccountTxInfo describle the tx info contained by the RetDetailAccountInfo
@@ -280,7 +281,7 @@ func createRetSimpleAccountInfo(account *database.DBAccount, ttBalance int64) *R
 	ret.Address = account.Address
 	ret.Balance = account.Balance
 	ret.TxCount = account.TxCount
-	ret.Percentage = (float64(ret.Balance) / float64(ttBalance))
+	ret.Percentage = (float64(ret.Balance) / 100000000) / 1000000000 //Fan turn seele, divided by total
 	ret.ShardNumber = account.ShardNumber
 	return &ret
 }
@@ -290,6 +291,7 @@ func createHomeRetSimpleAccountInfo(account *database.DBAccount) *RetSimpleAccou
 	var ret RetSimpleAccountHome
 	ret.Address = account.Address
 	ret.Balance = account.Balance
+	ret.Percentage = (float64(ret.Balance) / 100000000) / 1000000000 //Fan turn seele, divided by total
 	return &ret
 }
 
