8efdf390df71fcfbe9bb5795339814465d440eef
lizhengwei88 <408212402@qq.com>
2018-11-16 11:28:01 +0800

    syncer

--- syncer/syncer.go
+++ syncer/syncer.go
@@ -60,26 +60,20 @@ func (s *Syncer) checkOlderBlocks() bool {
 		log.Error(err)
 		return false
 	}
-
 	if dbBlockHeight == 0 {
 		return false
 	}
 
 	fallBack := false
 	log.Debug("checkOlderBlocks begin-------")
-	for i := dbBlockHeight - 1; i >= 0; i-- {
-		rpcBlock, err := s.rpc.GetBlockByHeight(i, true)
-		if err != nil {
-			return fallBack
-		}
+	for i := dbBlockHeight - 1; i >= dbBlockHeight-1; i-- {
+		// rpcBlock, err := s.rpc.GetBlockByHeight(i, true)
+		// if err != nil {
+		// 	return fallBack
+		// }
 
 		dbBlock, err := s.db.GetBlockByHeight(s.shardNumber, i)
 		if err != nil {
-			return fallBack
-		}
-
-		//if the block data is different wo should sync the data again
-		if dbBlock.HeadHash == rpcBlock.Hash {
 			return fallBack
 		}
 
@@ -89,104 +83,6 @@ func (s *Syncer) checkOlderBlocks() bool {
 
 		//Delete txs
 		s.db.RemoveTxs(s.shardNumber, i)
-
-		//Modify accounts
-		for j := 0; j < len(dbBlock.Txs); j++ {
-			tx := dbBlock.Txs[j]
-
-			if tx.To != "" {
-				toAccount, err := s.db.GetAccountByAddress(tx.To)
-				if err != nil {
-					log.Error(err)
-					return fallBack
-				}
-
-				toAccount.Balance, err = s.rpc.GetBalance(tx.To)
-				if err != nil {
-					log.Error(err)
-					toAccount.Balance = 0
-				}
-
-				txCnt, err := s.db.GetTxCntByShardNumberAndAddress(s.shardNumber, tx.To)
-				if err != nil {
-					log.Error(err)
-					txCnt = 0
-				}
-
-				toAccount.TxCount = int64(txCnt)
-				s.db.UpdateAccount(toAccount)
-			} else {
-				receipt, err := s.rpc.GetReceiptByTxHash(tx.Hash)
-				if err == nil {
-					contractAddress := receipt.ContractAddress
-
-					contractAccount, err := s.db.GetAccountByAddress(contractAddress)
-					if err != nil {
-						log.Error(err)
-						return fallBack
-					}
-
-					contractAccount.Balance, err = s.rpc.GetBalance(contractAddress)
-					if err != nil {
-						log.Error(err)
-						contractAccount.Balance = 0
-					}
-
-					txCnt, err := s.db.GetTxCntByShardNumberAndAddress(s.shardNumber, contractAddress)
-					if err != nil {
-						log.Error(err)
-						txCnt = 0
-					}
-
-					contractAccount.TxCount = int64(txCnt)
-					s.db.UpdateAccount(contractAccount)
-				}
-			}
-
-			if tx.From != nullAddress {
-				fromAccount, err := s.db.GetAccountByAddress(tx.From)
-
-				fromAccount.Balance, err = s.rpc.GetBalance(tx.From)
-				if err != nil {
-					log.Error(err)
-					fromAccount.Balance = 0
-				}
-
-				txCnt, err := s.db.GetTxCntByShardNumberAndAddress(s.shardNumber, tx.From)
-				if err != nil {
-					log.Error(err)
-					txCnt = 0
-				}
-
-				fromAccount.TxCount = int64(txCnt)
-				s.db.UpdateAccount(fromAccount)
-			}
-		}
-
-		if dbBlock.Creator != nullAddress {
-			minerAccount, err := s.db.GetAccountByAddress(dbBlock.Creator)
-			if err != nil {
-				minerAccount = database.CreateEmptyAccount(dbBlock.Creator, s.shardNumber)
-				err := s.db.AddAccount(minerAccount)
-				if err != nil {
-					log.Error("[DB] err : %v", err)
-				} else {
-					minerAccount, err = s.db.GetAccountByAddress(dbBlock.Creator)
-					if err != nil {
-						log.Error("[DB] err : %v", err)
-					}
-				}
-			}
-
-			blockCnt, err := s.db.GetMinedBlocksCntByShardNumberAndAddress(s.shardNumber, dbBlock.Creator)
-			if err != nil {
-				log.Error(err)
-				blockCnt = 0
-			}
-
-			s.db.UpdateAccountMinedBlock(dbBlock.Creator, blockCnt)
-		}
-
 		fallBack = true
 	}
 	log.Info("checkOlderBlocks end-------")
