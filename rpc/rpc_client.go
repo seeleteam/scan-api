186b6571ec9d923582a4b24ae31924eab3fee745
avadesian <chunxiangxu@gmail.com>
2019-05-15 22:49:46 +0800

    reconnect rpc every 10 secs if the connection is nil

--- rpc/rpc_client.go
+++ rpc/rpc_client.go
@@ -5,6 +5,11 @@
 
 package rpc
 
+import (
+	"github.com/seeleteam/scan-api/log"
+	"time"
+)
+
 // SeeleRPC json_rpc client
 type SeeleRPC struct {
 	url    string
@@ -45,6 +50,14 @@ func (rpc *SeeleRPC) Release() {
 }
 
 func (rpc *SeeleRPC) call(serviceMethod string, args interface{}, reply interface{}) error {
+	if rpc.conn == nil {
+		log.Error("rpc_client conn is nil, try to reconnect")
+		for rpc.Connect() !=nil {
+			time.Sleep(10*time.Second)
+			log.Error("rpc_client conn is nil, try to reconnect")
+			continue;
+		}
+	}
 	err := rpc.conn.Call(serviceMethod, args, &reply)
 	if err != nil {
 		return err
